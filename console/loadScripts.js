5dd295cd25eb913c0836b0b1060bbafb19775955
CTPache <33907485+CTPache@users.noreply.github.com>
2024-11-11 18:38:22 +0100

    Readme

--- console/loadScripts.js
+++ console/loadScripts.js
@@ -1,5 +1,5 @@
 sendConsole('load scripts/screenshot.js')
-//sendConsole('load scripts/mitm.js').then(() => sendConsole('mitm'))
+sendConsole('load scripts/mitm.js').then(() => sendConsole('mitm'))
 sendConsole('load scripts/logger.js')
 sendConsole('load scripts/link2fetch.js')
 sendConsole('load scripts/downloadFile.js')
